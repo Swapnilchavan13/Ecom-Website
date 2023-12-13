2eac267d30b6e4bf419971c616da2f9c4864b80d
swapnil chavan <chavanswapnil822@gmail.com>
2023-12-13 14:36:00 +0530

    update

--- ecom/src/Components/Login.jsx
+++ ecom/src/Components/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import '../Styles/login.css';
 import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
-import { Userdetails } from './Userdetails'
+import { Userdetails } from './Userdetails.jsx'
 import 'react-toastify/dist/ReactToastify.css';
 
 export const Login = () => {
@@ -68,7 +68,6 @@ export const Login = () => {
   return (
     <div id='logindiv'>
       {isloggedin ? (
-        // <Userdetails />
         <Userdetails />
       ) : (
         <>
