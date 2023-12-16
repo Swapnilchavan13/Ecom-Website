7c308257e323bc6f844a92e301b597dce271e51b
swapnil chavan <chavanswapnil822@gmail.com>
2023-12-16 13:38:47 +0530

    update

--- ecom/src/Components/Merchantorders.jsx
+++ ecom/src/Components/Merchantorders.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Navbarmarchant } from './Navbarmarchant';
+import '../Styles/merchantorder.css';
 
 export const Merchantorders = () => {
-  const [mercahntId, setMerchantId] = useState(""); // State to store the merchantId
+  const [merchantId, setMerchantId] = useState(""); // State to store the merchantId
   const [orders, setOrders] = useState([]); // State to store orders
 
   useEffect(() => {
@@ -26,19 +27,34 @@ export const Merchantorders = () => {
         })
         .catch(error => console.error('Error fetching orders:', error));
     }
-  }, []); // Empty dependency array means this effect will only run once on component mount
+  }, []); 
+  // Empty dependency array means this effect will only run once on component mount
 
   return (
     <>
       <Navbarmarchant />
-      <div>
         <h2>Merchant Orders</h2>
+      <div className="merchant-orders-container">
         {orders.map(order => (
-          <div key={order._id}>
+          <div key={order._id} className="order-card">
             {/* Display relevant order details here */}
-            <p>Order ID: {order._id}</p>
-            <p>Total: {order.total}</p>
-            {/* Add more details as needed */}
+            <p className="order-info">Order ID: {order._id}</p>
+            <p className="order-info">User ID: {order.userId}</p>
+            <p className="order-info">Order Date: {new Date(order.orderdate).toLocaleString()}</p>
+            <p className="order-info">Address: {order.address}</p>
+            <p className="order-info">Payment Method: {order.paymentMethod}</p>
+            <p className="order-info">Total: {order.total}</p>
+            <p className="order-info">Status: {order.status ? 'Completed' : 'Pending'}</p>
+
+            <h3 className="product-heading">Products:</h3>
+            {order.products.map(product => (
+              <div key={product._id} className="product-card">
+                <p className="product-info">Product Name: {product.productName}</p>
+                <img src={product.productImage} alt="" className="product-image" />
+                <p className="product-info">Quantity: {product.quantity}</p>
+                <p className="product-info">Price: {product.price}</p>
+              </div>
+            ))}
           </div>
         ))}
       </div>
