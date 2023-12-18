22707e32158805a4b8ed065c1921783ce5c7fe87
swapnil chavan <chavanswapnil822@gmail.com>
2023-12-18 11:36:17 +0530

    update

--- ecom/src/Components/Merchantorders.jsx
+++ ecom/src/Components/Merchantorders.jsx
@@ -1,72 +1,6 @@
-// import React, { useEffect, useState } from 'react';
-// import { Navbarmarchant } from './Navbarmarchant';
-// import '../Styles/merchantorder.css';
-
-// export const Merchantorders = () => {
-//   const [merchantId, setMerchantId] = useState(""); // State to store the merchantId
-//   const [orders, setOrders] = useState([]); // State to store orders
-
-//   useEffect(() => {
-//     // Step 1: Retrieve merchantId from local storage
-//     const storedMerchantId = localStorage.getItem('merchantid');
-//     if (storedMerchantId) {
-//       setMerchantId(storedMerchantId);
-
-//       // Step 2: Make a request to the API endpoint
-//       fetch('http://localhost:3008/allorders')
-//         .then(response => response.json())
-//         .then(data => {
-//           // Step 3: Filter orders based on merchantId
-//           const filteredOrders = data.filter(order => {
-//             if (order.products && order.products.length > 0) {
-//               return order.products.some(product => product.merchantId === storedMerchantId);
-//             }
-//             return false;
-//           });
-//           setOrders(filteredOrders);
-//         })
-//         .catch(error => console.error('Error fetching orders:', error));
-//     }
-//   }, []); 
-//   // Empty dependency array means this effect will only run once on component mount
-
-//   return (
-//     <>
-//       <Navbarmarchant />
-//         <h2>Merchant Orders</h2>
-//       <div className="merchant-orders-container">
-//         {orders.map(order => (
-//           <div key={order._id} className="order-card">
-//             {/* Display relevant order details here */}
-//             <p className="order-info">Order ID: {order._id}</p>
-//             <p className="order-info">User ID: {order.userId}</p>
-//             <p className="order-info">Order Date: {new Date(order.orderdate).toLocaleString()}</p>
-//             <p className="order-info">Address: {order.address}</p>
-//             <p className="order-info">Payment Method: {order.paymentMethod}</p>
-//             <p className="order-info">Total: {order.total}</p>
-//             <p className="order-info">Status: {order.status ? 'Completed' : 'Pending'}</p>
-
-//             <h3 className="product-heading">Products:</h3>
-//             {order.products.map(product => (
-//               <div key={product._id} className="product-card">
-//                 <p className="product-info">Product Name: {product.productName}</p>
-//                 <img src={product.productImage} alt="" className="product-image" />
-//                 <p className="product-info">Quantity: {product.quantity}</p>
-//                 <p className="product-info">Price: {product.price}</p>
-//               </div>
-//             ))}
-//           </div>
-//         ))}
-//       </div>
-//     </>
-//   );
-// };
-
-
-
 import React, { useState, useEffect } from 'react';
-// import '../styles/order.css';
 import { Navbarmarchant } from './Navbarmarchant';
+import '../Styles/merchantorder.css';
 
 export const Merchantorders = () => {
   const [orders, setOrders] = useState([]);
@@ -74,16 +8,10 @@ export const Merchantorders = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [merchantId, setMerchantId] = useState(""); // State to store the merchantId
 
-
   useEffect(() => {
     const fetchOrders = async () => {
       try {
-        // Fetch orders data
-        // const ordersResponse = await fetch('http://62.72.59.146:3008/allorders');
-        // const ordersData = await ordersResponse.json();
-        // setOrders(ordersData);
-
-               const storedMerchantId = localStorage.getItem('merchantid');
+          const storedMerchantId = localStorage.getItem('merchantid');
             if (storedMerchantId) {
                setMerchantId(storedMerchantId);
           
@@ -230,6 +158,7 @@ export const Merchantorders = () => {
               </td>
               <td>
                 <button onClick={() => handleDelete(order._id)}>Delete</button>
+                
                 <button onClick={() => handleStatusChange(order._id)}>Delivered</button>
               </td>
             </tr>
--- ecom/src/Components/Navbarmarchant.jsx
+++ ecom/src/Components/Navbarmarchant.jsx
@@ -4,19 +4,22 @@ import { Link } from 'react-router-dom'
 export const Navbarmarchant = () => {
   return (
     <div className='nav'>
-    <Link to="/merchantdatapage">
-      <h3>Deatails</h3>
+      <Link to="/merchantdatapage">
+        <h3>Deatails</h3>
       </Link>
       <Link to="/merchantdatapage/merchantadd">
-      <h3>Add Products</h3>
+        <h3>Add Products</h3>
       </Link>
       <Link to="/merchantdatapage/merchantproduct">
-      <h3>All Products</h3>
+        <h3>All Products</h3>
       </Link>
       <Link to="/merchantdatapage/merchantorder">
-      <h3>All Orders</h3>
+        <h3>All Orders</h3>
       </Link>
+      <a href="http://62.72.59.146:3001/" target="_blank" rel="noopener noreferrer">
+        <h3>Campaign Registration</h3>
+      </a>
     </div>
 
-    )
+  )
 }
